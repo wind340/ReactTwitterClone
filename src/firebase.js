b0992a6445c6059fba9b12ae2709ec94b1114b19
Jungchul_kim <83698052+wind340@users.noreply.github.com>
2022-05-04 00:09:32 +0900

    환경변수로 KEY 감추기

--- src/firebase.js
+++ src/firebase.js
@@ -7,13 +7,13 @@ import { getAnalytics } from "firebase/analytics";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyCWtlKqxByVc6Gu0IQHStzny6SACxxW5MU",
-  authDomain: "reacttwitterclone-f1112.firebaseapp.com",
-  projectId: "reacttwitterclone-f1112",
-  storageBucket: "reacttwitterclone-f1112.appspot.com",
-  messagingSenderId: "384740992103",
-  appId: "1:384740992103:web:b88a26961859a7ad5ae0c2",
-  measurementId: "G-NB1PP0E57K",
+  apiKey: process.env.REACT_APP_API_KEY,
+  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_MESSAGIN_ID,
+  appId: process.env.REACT_APP_APP_ID,
+  measurementId: process.env.REACT_APP_MEASUREMENT_ID,
 };
 
 // Initialize Firebase
