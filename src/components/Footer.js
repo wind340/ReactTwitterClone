9fe999260a1237607f1938365eb10806c97a0b47
Jungchul_kim <83698052+wind340@users.noreply.github.com>
2022-05-04 02:03:21 +0900

    임시 Footer 페이지 생성

---
+++ src/components/Footer.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Footer() {
+  return (
+    <div className="Footer">
+      <p> &copy; {new Date().getFullYear()}</p>
+    </div>
+  );
+}
+
+export default Footer;
