c252e128d8a17b1ec51b81210bfd723f3f17ff72
Jungchul_kim <83698052+wind340@users.noreply.github.com>
2022-05-05 23:57:27 +0900

    Update Auth.js

--- src/routes/Auth.js
+++ src/routes/Auth.js
@@ -21,7 +21,7 @@ const Auth = () => {
       <form onSubmit={onSubmit}>
         <input
           name="email"
-          type="text"
+          type="email"
           placeholder="Email"
           required
           value={email}
