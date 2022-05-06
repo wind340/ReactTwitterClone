133b2b1f03fd88ec8d1bc13b9336c40459e32d58
Jungchul_kim <83698052+wind340@users.noreply.github.com>
2022-05-06 18:11:24 +0900

    로직 간결하게 바꾸기

--- src/index.js
+++ src/index.js
@@ -1,10 +1,11 @@
 import React from "react";
-import ReactDOM from "react-dom/client";
+import ReactDOM from "react-dom";
 import App from "./components/App";
+import firebase from "./fbase";
 
-const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(
+ReactDOM.render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>
+  </React.StrictMode>,
+  document.getElementById("root")
 );
