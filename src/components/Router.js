ef3fe2619cd37e3f75e68bf7d4155e9873fa8952
Jungchul_kim <83698052+wind340@users.noreply.github.com>
2022-05-07 23:46:08 +0900

    profile route 추가

--- src/components/Router.js
+++ src/components/Router.js
@@ -2,14 +2,18 @@ import React from "react";
 import { HashRouter as Router, Route, Routes } from "react-router-dom";
 import Auth from "../routes/Auth";
 import Home from "../routes/Home";
+import Navigation from "components/Navigation";
+import Profile from "routes/Profile";
 
 const AppRouter = ({ isLoggedIn }) => {
   return (
     <Router>
+      {isLoggedIn && <Navigation />}
       <Routes>
         {isLoggedIn ? (
           <>
             <Route exact={true} path={"/"} element={<Home />}></Route>
+            <Route exact={true} path={"/Profile"} element={<Profile />}></Route>
           </>
         ) : (
           <Route exact={true} path={"/"} element={<Auth />}></Route>
