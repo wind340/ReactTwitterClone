3a29f10b904664467cf7f881f52157bc07a3150a
Jungchul_kim <83698052+wind340@users.noreply.github.com>
2022-05-07 23:46:51 +0900

    social login provider 추가

--- src/routes/Auth.js
+++ src/routes/Auth.js
@@ -4,6 +4,9 @@ import {
   getAuth,
   createUserWithEmailAndPassword,
   signInWithEmailAndPassword,
+  GoogleAuthProvider,
+  GithubAuthProvider,
+  signInWithPopup,
 } from "firebase/auth";
 
 const Auth = () => {
@@ -45,7 +48,18 @@ const Auth = () => {
     }
   };
   const toggleAccount = () => setNewAccount((prev) => !prev);
-
+  const onSocialClick = async (event) => {
+    const {
+      target: { name },
+    } = event;
+    let provider;
+    if (name === "google") {
+      provider = new GoogleAuthProvider();
+    } else if (name === "github") {
+      provider = new GithubAuthProvider();
+    }
+    await signInWithPopup(authService, provider);
+  };
   return (
     <div>
       <form onSubmit={onSubmit}>
@@ -72,8 +86,12 @@ const Auth = () => {
       </form>
       <span onClick={toggleAccount}>{newAccount ? "Sign In" : "Create"}</span>
       <div>
-        <button>Continue with Google</button>
-        <button>Continue with Github</button>
+        <button onClick={onSocialClick} name="google">
+          Continue with Google
+        </button>
+        <button onClick={onSocialClick} name="github">
+          Continue with Github
+        </button>
       </div>
     </div>
   );
