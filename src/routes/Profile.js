c77a8b0c291e11ef9fc04a4072bf5a740302e3c1
Jungchul_kim <83698052+wind340@users.noreply.github.com>
2022-05-07 23:47:35 +0900

    Logout Logic & button 추가

--- src/routes/Profile.js
+++ src/routes/Profile.js
@@ -1,5 +1,15 @@
+import Navigation from "components/Navigation";
+import { authService } from "fbase";
 import React from "react";
+import { useNavigate } from "react-router-dom";
 
-const Profile = () => <span>Profile</span>;
+const Profile = () => {
+  const navigate = useNavigate();
+  const onLogOutClick = () => {
+    authService.signOut();
+    navigate("/");
+  };
+  return <button onClick={onLogOutClick}>Logout</button>;
+};
 
 export default Profile;
