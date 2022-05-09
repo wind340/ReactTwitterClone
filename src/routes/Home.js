c42968cc11fec4ddf6d1af03fb915ec414192e2f
Jungchul_kim <83698052+wind340@users.noreply.github.com>
2022-05-09 23:59:36 +0900

    DB Create & Read

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,9 +1,43 @@
-import React, { useState } from "react";
+import { dbService } from "fbase";
+import {
+  addDoc,
+  collection,
+  query,
+  onSnapshot,
+  orderBy,
+} from "firebase/firestore";
+import React, { useEffect, useState } from "react";
 
-const Home = () => {
+const Home = ({ userObj }) => {
   const [nweet, setNweet] = useState("");
-  const onSubmit = (event) => {
+  const [nweets, setNweets] = useState([]);
+
+  useEffect(() => {
+    const q = query(
+      collection(dbService, "nweets"),
+      orderBy("createdAt", "desc")
+    );
+    onSnapshot(q, (snapshot) => {
+      const nweetArr = snapshot.docs.map((document) => ({
+        id: document.id,
+        ...document.data(),
+      }));
+      setNweets(nweetArr);
+    });
+  }, []);
+
+  const onSubmit = async (event) => {
     event.preventDefault();
+    try {
+      const docRef = await addDoc(collection(dbService, "nweets"), {
+        text: nweet,
+        createdAt: Date.now(),
+        creatorId: userObj.uid,
+      });
+    } catch (e) {
+      console.error("Error adding document: ", e);
+    }
+    setNweet("");
   };
   const onChange = (event) => {
     const {
@@ -24,6 +58,14 @@ const Home = () => {
         />
         <input type="submit" value="Nweet" />
       </form>
+      <div>
+        {nweets.map((nweet) => (
+          <div key={nweet.id}>
+            <h4>{nweet.text}</h4>
+            <span>{new Date(nweet.createdAt).toLocaleString()}</span>
+          </div>
+        ))}
+      </div>
     </div>
   );
 };
