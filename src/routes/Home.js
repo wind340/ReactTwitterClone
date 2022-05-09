aff4eaa82722231683e715b41acd04dad242a7b5
Jungchul_kim <83698052+wind340@users.noreply.github.com>
2022-05-10 03:25:39 +0900

    Nweet component 분리 후 넘겨줄 값 입력

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,3 +1,4 @@
+import Nweet from "components/Nweet";
 import { dbService } from "fbase";
 import {
   addDoc,
@@ -60,10 +61,11 @@ const Home = ({ userObj }) => {
       </form>
       <div>
         {nweets.map((nweet) => (
-          <div key={nweet.id}>
-            <h4>{nweet.text}</h4>
-            <span>{new Date(nweet.createdAt).toLocaleString()}</span>
-          </div>
+          <Nweet
+            key={nweet.id}
+            nweetObj={nweet}
+            isOwner={nweet.createId === userObj.uid}
+          />
         ))}
       </div>
     </div>
