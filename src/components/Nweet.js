010d9c33b050fa03b9d086cd76cd509e636e6c7b
Jungchul_kim <83698052+wind340@users.noreply.github.com>
2022-05-12 18:20:40 +0900

    CRUD 추가

--- src/components/Nweet.js
+++ src/components/Nweet.js
@@ -1,16 +1,70 @@
-import React from "react";
+import { dbService, storageService } from "fbase";
+import { deleteDoc, doc, updateDoc } from "firebase/firestore";
+import { deleteObject, ref } from "firebase/storage";
+import { useState } from "react";
+const Nweet = ({ nweetObj, isOwner }) => {
+  const [editing, setEditing] = useState(false);
+  const [newNweet, setNewNweet] = useState(nweetObj.text);
 
-const Nweet = ({ nweetObj }) => (
-  <div>
-    <h4>{nweetObj.text}</h4>
-    <span>{new Date(nweetObj.createdAt).toLocaleString()}</span>
-    {isOwner && (
-      <>
-        <button>Delete Nweet</button>
-        <button>Edit Nweet</button>
-      </>
-    )}
-  </div>
-);
+  const NweetTextRef = doc(dbService, "nweets", `${nweetObj.id}`);
+  const urlRef = ref(storageService, nweetObj.attachmentUrl);
+
+  const onDeleteClick = async () => {
+    const ok = window.confirm("Are you sure you want to delete this nweet?");
+    if (ok) {
+      await deleteDoc(NweetTextRef);
+      if (urlRef !== null) {
+        await deleteObject(urlRef);
+      }
+    }
+  };
+  const toggleEditing = () => setEditing((prev) => !prev);
+  const onSubmit = async (event) => {
+    event.preventDefault();
+    await updateDoc(NweetTextRef, {
+      text: newNweet,
+    });
+    setEditing(false);
+  };
+  const onChange = (event) => {
+    const {
+      target: { value },
+    } = event;
+    setNewNweet(value);
+  };
+  return (
+    <div>
+      {editing ? (
+        <>
+          <form onSubmit={onSubmit}>
+            <input
+              type="text"
+              placeholder="Edit your nweet"
+              value={newNweet}
+              required
+              onChange={onChange}
+            />
+            <input type="submit" value="Update Nweet" />
+          </form>
+          <button onClick={toggleEditing}>Cancel</button>
+        </>
+      ) : (
+        <>
+          <h4>{nweetObj.text}</h4>
+          {nweetObj.attachmentUrl && (
+            <img src={nweetObj.attachmentUrl} width="120px" height="120px" />
+          )}
+          <h5>{new Date(nweetObj.createdAt).toLocaleString()}</h5>
+          {isOwner && (
+            <>
+              <button onClick={onDeleteClick}>Delete Nweet</button>
+              <button onClick={toggleEditing}>Edit Nweet</button>
+            </>
+          )}
+        </>
+      )}
+    </div>
+  );
+};
 
 export default Nweet;
