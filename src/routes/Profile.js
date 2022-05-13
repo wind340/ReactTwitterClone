29ea766ab83c2f9379afae13d206aeded965d644
Jungchul_kim <83698052+wind340@users.noreply.github.com>
2022-05-13 21:30:35 +0900

    프로필 정보 조회 쿼리

--- src/routes/Profile.js
+++ src/routes/Profile.js
@@ -1,9 +1,21 @@
-import Navigation from "components/Navigation";
-import { authService } from "fbase";
+import { authService, dbService } from "fbase";
+import { collection, getDocs, query, where } from "firebase/firestore";
 import React from "react";
 import { useNavigate } from "react-router-dom";
 
-const Profile = () => {
+const Profile = ({ userObj }) => {
+  const getMyNweets = async () => {
+    const q = query(
+      collection(dbService, "nweets"),
+      where("creatorId", "==", userObj.uid)
+    );
+    const querySnapshot = await getDocs(q);
+
+    querySnapshot.forEach((doc) => {
+      console.log(doc.id, "=>", doc.data());
+    });
+  };
+
   const navigate = useNavigate();
   const onLogOutClick = () => {
     authService.signOut();
