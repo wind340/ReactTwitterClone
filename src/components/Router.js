c5de310c68c448429aa9aa62eea72e74b8623f53
Jungchul_kim <83698052+wind340@users.noreply.github.com>
2022-05-20 20:50:07 +0900

    style오류로 인해 코드 제거후 css변경

--- src/components/Router.js
+++ src/components/Router.js
@@ -7,40 +7,27 @@ import Profile from "routes/Profile";
 
 const AppRouter = ({ refreshUser, isLoggedIn, userObj }) => {
   return (
-    <div
-      style={{
-        maxWidth: 890,
-        width: "100%",
-        margin: "0 auto",
-        marginTop: 80,
-        display: "flex",
-        justifyContent: "center",
-      }}
-    >
-      <Router>
-        {isLoggedIn && <Navigation userObj={userObj} />}
-        <Routes>
-          {isLoggedIn ? (
-            <>
-              <Route
-                exact={true}
-                path={"/"}
-                element={<Home userObj={userObj} />}
-              ></Route>
-              <Route
-                exact={true}
-                path={"/Profile"}
-                element={
-                  <Profile userObj={userObj} refreshUser={refreshUser} />
-                }
-              ></Route>
-            </>
-          ) : (
-            <Route exact={true} path={"/"} element={<Auth />}></Route>
-          )}
-        </Routes>
-      </Router>
-    </div>
+    <Router>
+      {isLoggedIn && <Navigation userObj={userObj} />}
+      <Routes>
+        {isLoggedIn ? (
+          <>
+            <Route
+              exact={true}
+              path={"/"}
+              element={<Home userObj={userObj} />}
+            ></Route>
+            <Route
+              exact={true}
+              path={"/Profile"}
+              element={<Profile userObj={userObj} refreshUser={refreshUser} />}
+            ></Route>
+          </>
+        ) : (
+          <Route exact={true} path={"/"} element={<Auth />}></Route>
+        )}
+      </Routes>
+    </Router>
   );
 };
 
