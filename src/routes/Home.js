c85e300def303345c7ace298b9a93359b704aeb5
Jungchul_kim <83698052+wind340@users.noreply.github.com>
2022-05-20 22:47:10 +0900

    Update Home.js

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,8 +1,9 @@
 import Nweet from "components/Nweet";
-import { dbService } from "fbase";
+import { authService, dbService } from "fbase";
 import { collection, query, onSnapshot, orderBy } from "firebase/firestore";
 import React, { useEffect, useState } from "react";
 import NweetFactory from "components/NweetFactory";
+import { onAuthStateChanged } from "firebase/auth";
 
 const Home = ({ userObj }) => {
   const [nweets, setNweets] = useState([]);
@@ -21,20 +22,18 @@ const Home = ({ userObj }) => {
   }, []);
 
   return (
-    <>
-      <div className="container">
-        <NweetFactory userObj={userObj} />
-        <div style={{ marginTop: 30 }}>
-          {nweets.map((nweet) => (
-            <Nweet
-              key={nweet.id}
-              nweetObj={nweet}
-              isOwner={nweet.creatorId === userObj.uid}
-            />
-          ))}
-        </div>
+    <div className="container">
+      <NweetFactory userObj={userObj} />
+      <div style={{ marginTop: 30 }}>
+        {nweets.map((nweet) => (
+          <Nweet
+            key={nweet.id}
+            nweetObj={nweet}
+            isOwner={nweet.creatorId === userObj.uid}
+          />
+        ))}
       </div>
-    </>
+    </div>
   );
 };
 
